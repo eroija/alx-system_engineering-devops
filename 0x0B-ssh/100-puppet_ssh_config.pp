ba17d23449e8e767a78699d3bbff3863475e4280
Eric <eroija@gmail.com>
2024-04-20 15:58:54 +0300

    100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,21 @@
+# Using puppet
+# Your SSH client configuration must be configured to use
+# the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse
+# to authenticate using a password
+
+include stdlib
+
+file_line { 'Refuse auth with password':
+  ensure => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'specify identity file path':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
