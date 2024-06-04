af71c7f9b5cbea11336ff6475152d1eb8fde13c6
Eric <eroija@gmail.com>
2024-06-04 12:31:24 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,6 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data', {})
-        subscribers = data.get('subscribers', 0)
-        return subscribers
+        print(response.json()['data']['subscribers'])
     else:
         return 0
