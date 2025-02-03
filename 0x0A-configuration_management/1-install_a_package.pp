9d5ed3f325f9bc8c200967712054ef8f07265506
Eric <eroija@gmail.com>
2025-02-03 10:51:43 +0300

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
 # This manifest installs flask package 2.1.0 from pip3.
+
+package { 'Python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
